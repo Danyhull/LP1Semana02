3016aef7b45687a4813a42f60fe977daa126979c
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 10:36:45 +0000

    Foi corrigida a variavel do raio

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,9 +9,9 @@ namespace Cilindro
             Console.Write("Indique uma altura para um cilindro: ");
             string al = Console.ReadLine();
             Console.Write("Indique um raio para um cilindro: ");
-            string raio = Console.ReadLine();
+            string ra = Console.ReadLine();
             float a = float.Parse(al);
-            float r = float.Parse(raio);
+            float r = float.Parse(ra);
             float v = MathF.PI*MathF.Pow(r,2)*a;
             float s = 2*MathF.PI*r*(r+a);
             Console.WriteLine($"Volume do cilindro {v}");
