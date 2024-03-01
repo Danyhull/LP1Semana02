7b2f863badb74859df57e12d95f56626c05a784c
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 10:32:15 +0000

    Foi pedido ao utilizador que inseri-se uma altura a e um raio r

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indique uma altura para um cilindro: ");
+            string area = Console.ReadLine();
+            Console.Write("Indique um raio para um cilindro: ");
+            string rai = Console.ReadLine();
+        }
+    }
+}
