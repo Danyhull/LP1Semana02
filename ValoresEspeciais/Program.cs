3d0c9b16b778e3b3368c05578fdfd3c6f71526c0
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 10:59:02 +0000

    Foi criado o minimo e o máximo de todas as variaveis

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Todas as variáveis
+            //sbyte
+            sbyte sbytemx = sbyte.MaxValue;
+            sbyte sbytemn = sbyte.MinValue;
+
+            //short
+            short shortmx = short.MaxValue;
+            short shortmn = short.MinValue;
+
+            //int
+            int intmx = int.MaxValue;
+            int intmn = int.MinValue;
+
+            //long
+            long longmx = long.MaxValue;
+            long longmn = long.MinValue;
+
+            //byte
+            byte bytemx = byte.MaxValue;
+            byte bytemn = byte.MinValue;
+
+            //ushort
+            ushort ushortmx = ushort.MaxValue;
+            ushort ushortmn = ushort.MinValue;
+
+            //uint
+            uint uintmx = uint.MaxValue;
+            uint uintmn = uint.MinValue;
+
+            //ulong
+            ulong ulongmx = ulong.MaxValue;
+            ulong ulongmn = ulong.MinValue;
+
+            //char
+            char charmx = char.MaxValue;
+            char charmn = char.MinValue;
+
+            //float
+            float floatmx = float.MaxValue;
+            float floatmn = float.MinValue;
+
+            //double
+            double doublemx = double.MaxValue;
+            double doublemn = double.MinValue;
+
+            //decimal
+            decimal decimalmx = decimal.MaxValue;
+            decimal decimalmn = decimal.MinValue;
+
+            
+
+        }
+    }
+}
