bd5e98bf251d40426c2d82cbdc1599df85e9c5f7
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 10:17:27 +0000

    Criou-se um programa que pede ao utilizador um número inteiro não-negativo, e que faz a conversão e guarda o valor

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não-negativo: ");
+            string bit = Console.ReadLine();
+        }
+    }
+}
