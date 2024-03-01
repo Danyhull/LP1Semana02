4d48d6385bc34aa5bf072468f7c192423044863b
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 10:34:56 +0000

    Apresente o volume V e a área de superfície Sa desse cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,9 +7,15 @@ namespace Cilindro
         static void Main(string[] args)
         {
             Console.Write("Indique uma altura para um cilindro: ");
-            string area = Console.ReadLine();
+            string al = Console.ReadLine();
             Console.Write("Indique um raio para um cilindro: ");
-            string rai = Console.ReadLine();
+            string raio = Console.ReadLine();
+            float a = float.Parse(al);
+            float r = float.Parse(raio);
+            float v = MathF.PI*MathF.Pow(r,2)*a;
+            float s = 2*MathF.PI*r*(r+a);
+            Console.WriteLine($"Volume do cilindro {v}");
+            Console.WriteLine($"area da superficie do cilindro {s}");
         }
     }
 }
