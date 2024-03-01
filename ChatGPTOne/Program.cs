eb7b7d6c22e1dbc1418c33d1c18f03e106728b35
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-01 14:42:34 +0000

    Foram criadas respostas para certas perguntas do utilizador e tambem para aquelas que ele não conhece

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -14,6 +14,33 @@ namespace ChatGPTOne
             while (exit == true){
                 Console.Write("Place your question? ");
                 question = Console.ReadLine();
+
+            // Respostas as perguntas
+                switch (question)
+                {
+                    case "EXIT":
+                        response = "";
+                        exit = false;
+                        break;
+                    case "Who are you?":
+                        response = "I`m Zé Castelo Branco!";
+                        break;
+                    case "How can i be better in school?":
+                        response = "Use more ChatGPT but be careful!";
+                        break;
+                    case "Where do u live?":
+                        response = "I don't live in Amadora!";
+                        break;
+                    case "How can i be intelligent like you?":
+                        response = "U can't!";
+                        break;
+                    default:
+                        response = "I'am sorry but that question i can't answer";
+                        break;
+                }
+
+                // Responde adequadamente
+                Console.WriteLine(response);
         }
     }
 }
